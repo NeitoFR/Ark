e9f7e9864119b7b4313acf1e4b16b46395c0e35d
Neito <willem.houm@gmail.com>
2018-03-12 15:59:10 +0100

    Cleaning in progress

--- server/app.js
+++ server/app.js
@@ -14,7 +14,15 @@ app.use(express.static(path.join(__dirname, '/..', 'public')));
 //Routing
 app.get('/', function (req, res){
     res.sendFile('../public/index.html', {root: __dirname});
+    console.log(req.url);
 })
+/*app.get('/view*', function (req, res){
+    //var url = req.url.substring(1, req.url.length)+'.html';
+    //res.sendFile('../public/src/my-'+url, {root: __dirname});
+    res.writeHead(302, {'Location': '/'});
+    res.end();
+    console.log(req.url);
+})*/
 .listen(_port, function (){
     console.log('Server listening on : '+_port);
 });
