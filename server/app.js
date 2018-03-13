61402f9478bbb91d1ea8fa720bb2df723b1c4a25
Willem Houm <willem.houm@gmail.com>
2018-03-13 15:37:33 +0100

    Mid day 2

--- server/app.js
+++ server/app.js
@@ -1,28 +1,55 @@
 "use strict"
 //require
 var express = require('express'),
+bodyParser = require('body-parser'),
+session = require('express-session'),
 path = require('path');
 //Conf const
 const _port = 3000;
-
+//Session
+var sess = null;
 //Server
 var app = express();
 
 //Middleware
 app.use(express.static(path.join(__dirname, '/..', 'public')));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret:"azerty", resave:false, saveUninitialized:true}));
 //Routing
 app.get('/', function (req, res){
+    if(req.session.username)
+    {
+        res.send();
+    }
     res.sendFile('../public/index.html', {root: __dirname});
+
     console.log(req.url);
 })
-app.get('/view*', function (req, res){
+.get('/view*', function (req, res){
     //var url = req.url.substring(1, req.url.length)+'.html';
     //res.sendFile('../public/src/my-'+url, {root: __dirname});
     res.writeHead(302, {'Location': '/'});
     res.end();
     console.log(req.url);
 })
+.post('/login', function(req, res){
+    //Build Session
+    sess = req.session;
+    sess.user = req.body.user;
+    sess.pwd = req.body.pwd;
+    //Getting data
+    console.log("user received : ", req.body.user);
+    console.log("pwd received : ", req.body.pwd);
+    //DB Checking
+    
+    //Answer
+    res.writeHead(302, {'Location': '/'});
+    res.write({user: req.body.user});
+    res.end();
+})
+
+//Binding server to port
 .listen(_port, function (){
-    console.log('Server listening on : '+_port);
+    console.log('Server listening on  : '+_port);
 });
