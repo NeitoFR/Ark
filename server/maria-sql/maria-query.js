59e4e142c9a09ea103c0c11e69f3f3d6bf9aea5d
Willem Houm <willem.houm@gmail.com>
2018-05-22 00:30:15 +0200

    updating v_cur_step properly, cannot vote twice !

--- server/maria-sql/maria-query.js
+++ server/maria-sql/maria-query.js
@@ -2,7 +2,7 @@
 var Client = require('mariasql');
 require('dotenv').config();
 
-var init = function(base){
+var init = function (base) {
     //console.log(process.env);
     
     var c = new Client({
@@ -15,69 +15,109 @@ var init = function(base){
     return c;
 }
 
-exports.getUsers = function (callback){
+exports.getUsers = function (callback) {
     var c = init(process.env.B_USER);
     
-    c.query('SELECT * FROM Utilisateurs', function(err, res)
-    {
-        if (err){
+    c.query('SELECT * FROM Utilisateurs', function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.getUserInfo = function (id, callback){
+exports.getUserInfo = function (id, callback) {
     var c = init(process.env.B_USER);
-    var query = "SELECT id_Utilisateurs, pseudo, nom, prenom, nb_Alerte, nb_Projet, id_Groupe, email, phone, pays, ville, adresse FROM Utilisateurs WHERE id_Utilisateurs=\'"+id+"\';";
-    c.query(query, function(err, res)
-    {
-        if (err){
+    var query = "SELECT id_Utilisateurs, pseudo, nom, prenom, nb_Alerte, nb_Projet, id_Groupe, email, phone, pays, ville, adresse FROM Utilisateurs WHERE id_Utilisateurs=\'" + id + "\';";
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.sumbitParticipation = function (data, callback){
+exports.sumbitParticipation = function (data, callback) {
     var c = init(process.env.B_MISSION);
     // console.log(data);
     
     // var query = 'INSERT INTO `Missions` (`id_Utilisateurs`, `pseudo`, `nom`, `prenom`, `password`, `nb_Alerte`, `nb_Projet`, `id_Groupe`, `email`, `phone`, `pays`, `ville`, `adresse`, `visible`) \
     // VALUES (NULL, \''+data.pseudo+'\', \''+data.nom+'\', \''+data.prenom+'\', \''+data.password+'\', \'0\', \'0\', \'1\', \''+data.email+'\', \''+data.phone+'\', \''+data.country+'\', \''+data.city+'\', \''+data.address+'\', \'1\');';
-    var query = 'UPDATE Missions SET m_potentiel = CONCAT(m_potentiel, \''+data.string+'\') WHERE id_Mission=\''+data.id_Mission+'\';';
+    var query = 'UPDATE Missions SET m_potentiel = CONCAT(m_potentiel, \'' + data.string + '\') WHERE id_Mission=\'' + data.id_Mission + '\';';
     
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
+        } else {
+            c.end();
+            callback(null, res);
         }
-        else{
+    });
+}
+exports.submitAvis = function (data, callback) {
+    var c = init(process.env.B_MISSION);
+    // callback(null, 'Merci d\'avoir '+data.avis);
+    
+    // var query = 'INSERT INTO `Missions` (`id_Utilisateurs`, `pseudo`, `nom`, `prenom`, `password`, `nb_Alerte`, `nb_Projet`, `id_Groupe`, `email`, `phone`, `pays`, `ville`, `adresse`, `visible`) \
+    // VALUES (NULL, \''+data.pseudo+'\', \''+data.nom+'\', \''+data.prenom+'\', \''+data.password+'\', \'0\', \'0\', \'1\', \''+data.email+'\', \''+data.phone+'\', \''+data.country+'\', \''+data.city+'\', \''+data.address+'\', \'1\');';
+    // var query = 'UPDATE Missions SET m_potentiel = CONCAT(m_potentiel, \''+data.string+'\') WHERE id_Mission=\''+data.id_Mission+'\';';
+    
+    // c.query(query, function(err, res)
+    // {
+    //     if (err){
+    //         c.end();
+    //         callback(err, null);
+    //     }
+    //     else{
+    //         c.end();
+    //         callback(null, res);
+    //     }
+    // });
+}
+exports.updateVCurStep = function (id_Mission, v_cur_step, callback) {
+    console.log('id mission ' + id_Mission);
+    var c = init(process.env.B_MISSION);
+    var query = 'UPDATE missions SET `v_cur_step` = \''+v_cur_step+'\' WHERE id_Mission=\'' + id_Mission + '\';';
+    c.query(query, function (err, res) {
+        if (err) {
+            c.end();
+            callback(err, null);
+        } else {
+            c.end();
+            callback(null, res);
+        }
+    });
+}
+exports.getVCurStep = function (id_Mission, callback) {
+    // console.log('id mission ' + id_Mission);
+    var c = init(process.env.B_MISSION);
+    var query = 'SELECT v_cur_step FROM missions WHERE id_Mission=\'' + id_Mission + '\';';
+    c.query(query, function (err, res) {
+        if (err) {
+            c.end();
+            callback(err, null);
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
 //Authentication related query
-exports.createUser = function (data, callback){
+exports.createUser = function (data, callback) {
     var c = init(process.env.B_USER);
     var query = 'INSERT INTO `Utilisateurs` (`id_Utilisateurs`, `pseudo`, `nom`, `prenom`, `password`, `nb_Alerte`, `nb_Projet`, `id_Groupe`, `email`, `phone`, `pays`, `ville`, `adresse`, `visible`) \
-    VALUES (NULL, \''+data.pseudo+'\', \''+data.nom+'\', \''+data.prenom+'\', \''+data.password+'\', \'0\', \'0\', \'1\', \''+data.email+'\', \''+data.phone+'\', \''+data.country+'\', \''+data.city+'\', \''+data.address+'\', \'1\');';
+    VALUES (NULL, \'' + data.pseudo + '\', \'' + data.nom + '\', \'' + data.prenom + '\', \'' + data.password + '\', \'0\', \'0\', \'1\', \'' + data.email + '\', \'' + data.phone + '\', \'' + data.country + '\', \'' + data.city + '\', \'' + data.address + '\', \'1\');';
     //console.log(query);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
@@ -86,14 +126,12 @@ exports.createUser = function (data, callback){
 exports.verifyUser = function (data, callback) {
     var c = init(process.env.B_USER),
     
-    query = 'SELECT id_Utilisateurs, pseudo, nom, prenom, nb_Alerte, nb_Projet, id_Groupe, email, phone, pays, ville, adresse FROM Utilisateurs WHERE `pseudo` = \''+data.pseudo+'\' AND `password` = \''+data.password+'\';';
-    c.query(query, function(err, res)
-    {
-        if (err){
+    query = 'SELECT id_Utilisateurs, pseudo, nom, prenom, nb_Alerte, nb_Projet, id_Groupe, email, phone, pays, ville, adresse FROM Utilisateurs WHERE `pseudo` = \'' + data.pseudo + '\' AND `password` = \'' + data.password + '\';';
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
@@ -103,114 +141,103 @@ exports.verifyUser = function (data, callback) {
 exports.addComment = function (data, callback) {
     // console.log('data', data)
     var c = init(process.env.B_MISSION),
-    toAppend = '|'+Object.keys(data)[0]+"|"+data[Object.keys(data)[0]],
-    query = 'UPDATE Missions SET commentaires = CONCAT(commentaires, \''+toAppend+'\') WHERE id_Mission=\''+data.id+'\';';
+    toAppend = '|' + Object.keys(data)[0] + "|" + data[Object.keys(data)[0]],
+    query = 'UPDATE Missions SET commentaires = CONCAT(commentaires, \'' + toAppend + '\') WHERE id_Mission=\'' + data.id + '\';';
     
     //console.log('comment to append : ', toAppend);
-    c.query(query, function(err, res){
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.getCommentThread = function (id, callback){
+exports.getCommentThread = function (id, callback) {
     var c = init(process.env.B_MISSION),
-    query ="SELECT commentaires FROM Missions WHERE id_Mission=\'"+id+"\';";
+    query = "SELECT commentaires FROM Missions WHERE id_Mission=\'" + id + "\';";
     
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.getAlerts = function (wanted, callback){
+exports.getAlerts = function (wanted, callback) {
     var query;
     
     switch (wanted) {
         case "all":
-            query = "SELECT id_Mission, nom, resume FROM Missions"; 
+        query = "SELECT id_Mission, nom, resume FROM Missions";
         break;
-
+        
         case "new":
         
         break;
-
+        
         default:
         break;
     }
     var c = init(process.env.B_MISSION);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.createAlert = function (data, callback){
+exports.createAlert = function (data, callback) {
     var c = init(process.env.B_MISSION);
     // console.log(data.log);
     
     var query = 'INSERT INTO `Missions` (`id_Mission`, `nom`, `resume`, `date_creation`, `continent`, `pays`, `ville`, `id_Espece`, `id_Status`, `contenu`, `commentaires`, `initiateur`, `v_cur_step`, `m_potentiel`,`m_valider`, `donation`, `retour`,`activity_log`, `visible`)  \
-    VALUES (NULL, \''+data.alertName+'\', \''+data.summary+'\', \'2018-04-12\', \''+data.continent+'\', \''+data.country+'\', \''+data.city+'\', \''+data.id_Espece+'\', \'1\', \''+data.desc+'\', \'\', \''+data.initiateur+'\', \'||\', \'\', \'\', \'0\', \'0\',\''+data.log+'\', \'0\');';
+    VALUES (NULL, \'' + data.alertName + '\', \'' + data.summary + '\', \'2018-04-12\', \'' + data.continent + '\', \'' + data.country + '\', \'' + data.city + '\', \'' + data.id_Espece + '\', \'1\', \'' + data.desc + '\', \'\', \'' + data.initiateur + '\', \'||\', \'\', \'\', \'0\', \'0\',\'' + data.log + '\', \'0\');';
     // VALUES (NULL, \''+data.alertName+'\', \''+data.summary+'\', \'2018-04-12\', \''+data.continent+'\', \''+data.country+'\', \''+data.city+'\', \'212\', \'1\', \'\', \'\', \''+data.desc+'\', \'\', \'\', \'\', \'0\', \'0\', \'0\');';
     //console.log(query);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.getAlertById = function (id, callback){
+exports.getAlertById = function (id, callback) {
     var c = init(process.env.B_MISSION);
     
-    var query = 'SELECT * FROM Missions WHERE id_Mission=\''+id+'\';';
+    var query = 'SELECT * FROM Missions WHERE id_Mission=\'' + id + '\';';
     //console.log(query);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
     });
 }
-exports.getEspece = function (id, callback){
+exports.getEspece = function (id, callback) {
     var c = init(process.env.B_TAXO);
     
-    var query = 'SELECT espece FROM Espece WHERE id_Espece =\''+id+'\';';
+    var query = 'SELECT espece FROM Espece WHERE id_Espece =\'' + id + '\';';
     //console.log(query);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             // console.log("animal", res);
             
             c.end();
@@ -218,18 +245,16 @@ exports.getEspece = function (id, callback){
         }
     });
 }
-exports.getCommentFromMissionId = function (id, callback){
+exports.getCommentFromMissionId = function (id, callback) {
     var c = init(process.env.B_MISSION);
     
-    var query = 'SELECT commentaires FROM Missions WHERE id_Mission =\''+id+'\';';
+    var query = 'SELECT commentaires FROM Missions WHERE id_Mission =\'' + id + '\';';
     //console.log(query);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             // console.log("animal", res);
             
             c.end();
@@ -238,18 +263,16 @@ exports.getCommentFromMissionId = function (id, callback){
     });
 }
 
-exports.getAnimals = function (callback){
+exports.getAnimals = function (callback) {
     var c = init(process.env.B_TAXO);
     
     var query = 'SELECT espece, id_Espece FROM Espece;';
     //console.log(query);
-    c.query(query, function(err, res)
-    {
-        if (err){
+    c.query(query, function (err, res) {
+        if (err) {
             c.end();
             callback(err, null);
-        }
-        else{
+        } else {
             c.end();
             callback(null, res);
         }
--- server/maria-sql/maria-routes.js
+++ server/maria-sql/maria-routes.js
@@ -1,97 +1,88 @@
 var express = require('express'),
-maria  = require('./maria-query');
+  maria = require('./maria-query');
 
 var app = module.exports = express.Router();
 
-app.get('/maria/users', function(req, res) {
-  maria.getUsers(function(err, data){
-    if(err){ 
+app.get('/maria/users', function (req, res) {
+  maria.getUsers(function (err, data) {
+    if (err) {
       //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
       res.status(400).send(err).end();
-    }
-    else{
+    } else {
       //console.log('Request : SELECT * FROM USER_TABLE :: OK');
       res.status(200).send(data).end();
     }
   });
 });
-app.get('/maria/users/:id', function(req, res) {
-  maria.getUserInfo(req.params.id, function(err, data){
-    if(err){ 
+app.get('/maria/users/:id', function (req, res) {
+  maria.getUserInfo(req.params.id, function (err, data) {
+    if (err) {
       res.status(400).send(err).end();
-    }
-    else{
+    } else {
       res.status(200).send(data).end();
     }
   });
 });
 //Authentication related route
-app.post('/signup', function(req, res) {
-  maria.createUser(req.body, function(err, data){
-    if(err){ 
+app.post('/signup', function (req, res) {
+  maria.createUser(req.body, function (err, data) {
+    if (err) {
       var str = '';
       switch (err.code) {
         case 1062:
-        str = 'pseudo already taken';
-        break;
+          str = 'pseudo already taken';
+          break;
         case 1406:
-        str = 'pseudo or password too long';
-        break;
+          str = 'pseudo or password too long';
+          break;
         default:
-        str = 'Unexpected error code : '+err.code;
-        break;
+          str = 'Unexpected error code : ' + err.code;
+          break;
       }
       //console.log('Request : Create user :: ERROR : ', err);
       res.status(400).send(str).end();
-    }
-    else{
+    } else {
       //console.log('Request : Create user :: OK', data);
       res.status(200).send(data).end();
     }
   });
 });
-app.post('/signin', function(req, res) {
-  maria.verifyUser(req.body, function(err, data){
-    if(err){ 
+app.post('/signin', function (req, res) {
+  maria.verifyUser(req.body, function (err, data) {
+    if (err) {
       //console.log('Request : Verify user :: ERROR', err);
       res.status(400).send(err).end();
-    }
-    else{
-      
+    } else {
+
       //console.log('Request : Verify user :: OK\n data : ', data);
-      if(data.info.numRows == 0)
-      {
+      if (data.info.numRows == 0) {
         res.status(201).send('pseudo or password incorrect').end();
       }
-      if(data.info.numRows == 1)
-      {
+      if (data.info.numRows == 1) {
         res.status(200).send(data).end();
       }
     }
   });
 });
-app.post('/maria/add-comment', function(req, res){
+app.post('/maria/add-comment', function (req, res) {
   //console.log('comment : ', req.body[Object.keys(req.body)[0]]);
   //console.log('user : ', Object.keys(req.body)[0]);
-  maria.addComment(req.body, function(err, data){
-    if(err){ 
+  maria.addComment(req.body, function (err, data) {
+    if (err) {
       //console.log('Request : INSERT INTO mission (id_mission, commentaire) :: ERROR', err);
       res.status(400).send(err).end();
+    } else { //console.log('Request : Update commentaire :: OK');
+      //res.status(200).send('Commentaire ajouté').end();
+      res.status(204).send(data).end();
     }
-    else
-    {//console.log('Request : Update commentaire :: OK');
-    //res.status(200).send('Commentaire ajouté').end();
-    res.status(204).send(data).end();
-  }
+  });
 });
-});
-app.get('/maria/get-comments/:id', function(req, res) {
-  maria.getCommentThread(req.params.id, function(err, data){
-    if(err){ 
+app.get('/maria/get-comments/:id', function (req, res) {
+  maria.getCommentThread(req.params.id, function (err, data) {
+    if (err) {
       //console.log('Request : SELECT commentaire :: ERROR');
       res.status(400).send(err).end();
-    }
-    else{
+    } else {
       //console.log('Request : SELECT commentaire :: OK');
       var str = Object.values(data[0]);
       //console.log('thread', str[0]);
@@ -99,128 +90,129 @@ app.get('/maria/get-comments/:id', function(req, res) {
     }
   });
 });
-app.post('/missions/get', function(req, res) {
+app.post('/missions/get', function (req, res) {
   //console.log("body", req.body.wanted);
-  
-  maria.getAlerts(req.body.wanted, function(err, data){
-    if(err){ 
+
+  maria.getAlerts(req.body.wanted, function (err, data) {
+    if (err) {
       //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
       res.status(400).send(err).end();
-    }
-    else{
+    } else {
       //console.log('Request : SELECT * FROM USER_TABLE :: OK');
       res.status(200).send(data).end();
     }
   });
 });
-app.get('/missions/alert-status-1', function(req, res) {
-  maria.getAlertByStatus(1, function(err, data){
-    if(err){
+app.get('/missions/alert-status-1', function (req, res) {
+  maria.getAlertByStatus(1, function (err, data) {
+    if (err) {
       //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
       res.status(400).send(err).end();
-    }
-    else{
+    } else {
       //console.log('Request : SELECT * FROM USER_TABLE :: OK');
       res.status(200).send(data).end();
     }
   });
 });
-app.post('/maria/create-alert', function(req, res){
+app.post('/maria/create-alert', function (req, res) {
   //console.log(req.body);
-  
+
   //console.log('comment : ', req.body[Object.keys(req.body)[0]]);
   //console.log('user : ', Object.keys(req.body)[0]);
-  maria.createAlert(req.body, function(err, data){
-    if(err){ 
+  maria.createAlert(req.body, function (err, data) {
+    if (err) {
       // console.log('Request : INSERT INTO mission (id_mission, commentaire) :: ERROR', err);
       res.status(400).send(err).end();
-    }
-    else
-    {
+    } else {
       //console.log('Request : Update commentaire :: OK');
       //res.status(200).send('Commentaire ajouté').end();
       res.status(204).send(data).end();
     }
   });
 });
-app.post('/maria/submit-participation', function(req, res){
+app.post('/maria/submit-participation', function (req, res) {
   // console.log(req.body);
-  
-  maria.sumbitParticipation(req.body, function(err, data){
-    if(err){ 
+
+  maria.sumbitParticipation(req.body, function (err, data) {
+    if (err) {
       //console.log('Request : INSERT INTO mission (id_mission, commentaire) :: ERROR', err);
       res.status(400).send(err).end();
-    }
-    else
-    {
+    } else {
       //console.log('Request : Update commentaire :: OK');
       res.status(204).send(data).end();
     }
   });
 });
-// app.get('/missions/:project_id', function(req, res) {
-//   //console.log('url ', req.params);
-//   maria.getAlertById(req.params.project_id, function(err, data){
-//     if(err){
-//       //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
-//       res.status(400).send(err).end();
-//     }
-//     else{
-//       //console.log('Request : SELECT * FROM USER_TABLE :: OK');
-//       res.status(200).send(data).end();
-//     }
-//   });
-// });
-app.get('/missions/:project_id', function(req, res) {
+app.post('/maria/submit-avis', function (req, res) {
+  console.log(req.body);
+  maria.getVCurStep(req.body.id_Mission, function (err, data) {
+    if (err) {
+      //console.log('Request : INSERT INTO mission (id_mission, commentaire) :: ERROR', err);
+      res.status(400).send(err).end();
+    } else {
+      var voteurs = data[0].v_cur_step.split('|');
+      // console.log(voteurs[req.body.avis - 1]);
+      voteurs[req.body.avis - 1] += ';' + req.body.id_Utilisateurs;
+      voteurs = voteurs.join('|');
+      maria.updateVCurStep(req.body.id_Mission, voteurs, function (err, data) {
+        if (err) {
+          console.log(err);
+          
+          res.status(400).send(err).end();
+        } else {
+          res.status(200).send(data).end();
+        }
+      });
+    }
+  });
+});
+app.get('/missions/:project_id', function (req, res) {
   //console.log('url ', req.params);
   var result = [];
-  maria.getAlertById(req.params.project_id, function(err, data){
-    if(err){
+  maria.getAlertById(req.params.project_id, function (err, data) {
+    if (err) {
       //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
       res.status(400).send(err).end();
-    }
-    else{
+    } else {
       //console.log('Request : SELECT * FROM USER_TABLE :: OK');
       // res.status(200).send(data).end();
       result.push(data);
       // console.log(data[0].id_Espece);
-      
-      maria.getEspece(data[0].id_Espece, function(err, data){
-        if(err){
+
+      maria.getEspece(data[0].id_Espece, function (err, data) {
+        if (err) {
           //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
           res.status(400).send(err).end();
-        }
-        else{
+        } else {
           //console.log('Request : SELECT * FROM USER_TABLE :: OK');
           // console.log('animal ', data);
-          
+
           result.push(data);
           res.status(200).send(result).end();
-          
-        }});
-      }
-    });
+
+        }
+      });
+    }
   });
-  app.get('/comment/:id', function(req, res) {
-    maria.getCommentFromMissionId(req.params.id, function(err, data){
-      if(err){ 
-        res.status(400).send(err).end();
-      }
-      else{
-        res.status(200).send(data).end();
-      }
-    });
+});
+app.get('/comment/:id', function (req, res) {
+  maria.getCommentFromMissionId(req.params.id, function (err, data) {
+    if (err) {
+      res.status(400).send(err).end();
+    } else {
+      res.status(200).send(data).end();
+    }
   });
-  app.get('/maria/get-animals', function(req, res) {
-    //console.log('url ', req.params);
-    maria.getAnimals(function(err, data){
-      if(err){
-        //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
-        res.status(400).send(err).end();
-      }
-      else{
-        //console.log('Request : SELECT * FROM USER_TABLE :: OK');
-        res.status(200).send(data).end();
-      }
-    });
+});
+app.get('/maria/get-animals', function (req, res) {
+  //console.log('url ', req.params);
+  maria.getAnimals(function (err, data) {
+    if (err) {
+      //console.log('Request : SELECT * FROM USER_TABLE :: ERROR');
+      res.status(400).send(err).end();
+    } else {
+      //console.log('Request : SELECT * FROM USER_TABLE :: OK');
+      res.status(200).send(data).end();
+    }
   });
+});
